d1350cc6ed93053f46e1d924c65ece896788f6fb
Alias <yygggg@foxmail.com>
2022-09-30 01:23:07 +0800

    feat: add 63

---
+++ solution_1_100/problem63.py
@@ -0,0 +1,21 @@
+def solution() -> int:
+    """How many n-digit positive integers
+    exist which are also an nth power
+
+    Formula (9**n) == n
+    Returns:
+        _type_: _description_
+    """
+    count = 0
+    ans = []
+    for i in range(1, 21 + 1):
+        j = 9
+        while len(str(j**i)) == i and j**i > 0:
+            ans.append(f"{j}**{i}")
+            count += 1
+            j -= 1
+    return count
+
+
+if __name__ == "__main__":
+    print(solution())
