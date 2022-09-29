daa9965b8015976feef37ab39a2c9c95d6dc4a55
Alias <yygggg@foxmail.com>
2022-09-29 14:06:38 +0800

    fix : update the 250

--- solution_200_300/problem250.py
+++ solution_200_300/problem250.py
@@ -1,21 +1,35 @@
-def compute(n = 250):
-	# Use dynamic programming
+def compute(n=250):
+    """_summary_
+
+    Args:
+        n (int, optional): _description_. Defaults to 250.
+
+    Returns:
+        _type_: _description_
+    """
+    # Use dynamic programming
     MOD = 10**16
     subsets = [0] * n
     # subsets[i] is {the number of subsets with sum equal to i mod 250} mod 10^16
     subsets[0] = 1
     for i in range(1, 200000 + 1):
         offset = pow(i, i, n)
-        #print(offset)
-        subsets = [(val + subsets[(j - offset) % n]) % MOD for (j, val) in enumerate(subsets)]
+        # print(offset)
+        subsets = [
+            (val + subsets[(j - offset) % n]) % MOD for (j, val) in enumerate(subsets)
+        ]
     print(subsets)
     ans = (subsets[0] - 1) % MOD
     return str(ans)
 
 
 def solution():
+    """_summary_
+
+    Get the solution
+    """
     m = 250
-    mm = 10 ** 16
+    mm = 10**16
     x = m * [0]
 
     for i in range(1, 250251):
@@ -26,9 +40,10 @@ def solution():
 
     for i in range(m):
         for j in range(x[i]):
-            y = [(y[k] + y[(k-i)%m]) % mm for k in range(m)]
+            y = [(y[k] + y[(k - i) % m]) % mm for k in range(m)]
     print(y[0] - 1)
- 
+
+
 if __name__ == "__main__":
-    #print(compute())
+    # print(compute())
     solution()
